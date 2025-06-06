6104fca6330d4ff7e6e29b910b4cf235050b6731
Eugenio <eugenix86@gmail.com>
2025-06-06 22:34:41 +0200

    feat(cleaning): Complete initial data exploration and cleaning setup. Scripts now fully independent.

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,58 +1,93 @@
 import pandas as pd
 import geopandas as gpd
+from tabulate import tabulate
 import os
 import sys
 import logging
- 
 
-if __name__ == "__main__": 
-    
-    SRC_DIR = os.path.dirname(os.path.abspath(__file__))                                                                # Ottiene il percorso della cartella SRC_DIR
-    PROJECT_DIR = os.path.dirname(SRC_DIR)                                                                              # Ottiene il percorso della cartella del progetto
-    LOG_PATH = os.path.join(PROJECT_DIR,'logs','pipeline.log')                                                          # Percorso del file di log
-    
-    logging.basicConfig(                                                                                                # Set up logging
+
+if __name__ == "__main__":
+
+    SRC_DIR = os.path.dirname(os.path.abspath(__file__))                                                                                            # Ottiene il percorso della cartella SRC_DIR
+    PROJECT_DIR = os.path.dirname(SRC_DIR)                                                                                                          # Ottiene il percorso della cartella del progetto
+    LOG_PATH = os.path.join(PROJECT_DIR,'logs','pipeline.log')                                                                                      # Percorso del file di log
+    FILE_NAME = 'piste-ciclopedonali.parquet'                                                                                                       # Nome del file parquet
+    PATH_INPUT_PARQUET = os.path.join(PROJECT_DIR,'data','raw',FILE_NAME)                                                                           # Percorso del file parquet
+
+
+    logging.basicConfig(                                                                                                                            # Set up logging
         filename=LOG_PATH,
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         filemode='w'
     )
-    
-    logging.info("Clean Data: Inizio dell'esecuzione dello script di cleaning.")                                        # Messaggio di inizio dello script
-    
-    FILE_NAME = 'piste-ciclopedonali.parquet'                                                                           # Nome del file parquet
-    SRC_DIR = os.path.dirname(os.path.abspath(__file__))
-    PROJECT_DIR = os.path.dirname(SRC_DIR)                                                                              # Ottiene il percorso della cartella del progetto
-    PATH_INPUT_PARQUET = os.path.join(PROJECT_DIR,'data','raw',FILE_NAME)                                               # Percorso del file parquet
 
-    
-    
-    '''1. FASE DI CARICAMENTO DEL FILE PARQUET'''
-    
+    logging.info("Clean Data: Inizio dell'esecuzione dello script di cleaning....\n\n\n")                                                           # Messaggio di inizio dello script
+
+
+
+
+    '''1. FASE DI CARICAMENTO DEL FILE PARQUET---------------------------------------------------------------------------------------------------'''
+
     try:
-        gdf = gpd.read_parquet(PATH_INPUT_PARQUET)                                                                      # Carica il file geojson in un GeoDataFrame
+        gdf = gpd.read_parquet(PATH_INPUT_PARQUET)                                                                                                  # Carica il file parquet in un GeoDataFrame
     except FileNotFoundError as e:
-        logging.exception(f"Clean Data: ERRORE CRITICO - File parquet non trovato al percorso {PATH_INPUT_PARQUET}: {e}")                                                        # Controlla se il file esiste
+        logging.exception(f"Clean Data: ERRORE CRITICO - File parquet non trovato al percorso {PATH_INPUT_PARQUET}: {e}")                           # Controlla se il file esiste
         sys.exit(1)
-        
+
     except Exception as e:
-        logging.exception(f"Clean Data: Errore durante il caricamento del file {PATH_INPUT_PARQUET}: {e}")              # Gestione degli errori di lettura del file
-        raise e
-    
-    
-    
-    '''2. FASE DI LETTURA DEL FILE PARQUET'''
-    
-    logging.info("\n\n\n########Clean Data: File parquet caricato con successo - ANTEPRIMA FILE########################################")  
+        logging.exception(f"Clean Data: Errore durante il caricamento del file {PATH_INPUT_PARQUET}: {e}")                                          # Gestione degli errori di lettura del file
+        sys.exit(1)
+
+    '''2. FASE DI LETTURA DEL FILE PARQUET-------------------------------------------------------------------------------------------------------'''
+
+    logging.info("########Clean Data: File parquet caricato con successo - ANTEPRIMA FILE##########################################################")
     logging.info(f"Clean Data: Informazioni del file sorgente: \n")
     print(gdf.info())
-    logging.info(f"Clean Data: Nomi delle colonne del GeoDataFrame:{gdf.columns.tolist()}")                             # Messaggio di log per i nomi delle colonne
-    logging.info(f"Clean Data: N° di record vuoti:{gdf.isnull().sum()}")                                                # Messaggio di log per i record vuoti
-    logging.info(f"Clean Data: Anteprima del Geodataframe:{gdf.head()}")                                                # Messaggio di log per l'anteprima del GeoDataFrame   
-    logging.info(f"Clean Data: Shape del Geodataframe: {gdf.shape}")                                                    # Messaggio di log per la forma del GeoDataFrame
-    logging.info(f"Clean Data: Conteggio per tipologia record: \n\n{gdf['dtipologia2'].value_counts()}")                # Messaggio di log per il conteggio per tipologia record
-    logging.info("\n\n\n########Clean Data: File parquet FINE ANTEPRIMA FILE###########################################################\n\n\n")
-     
-    logging.info(f"Clean Data: Fine dell'esecuzione dello script.")                                                     # Messaggio di fine dello script
-  
+    logging.info(f"Clean Data: Nomi delle colonne del GeoDataFrame:{gdf.columns.tolist()}")                                                         # Messaggio di log per i nomi delle colonne
+    logging.info(f"Clean Data: N° di record vuoti:{gdf.isnull().sum()}")                                                                            # Messaggio di log per i record vuoti
+    logging.info(f"Clean Data: Anteprima del Geodataframe:{tabulate(gdf.head(), headers='keys', tablefmt='psql')}")                                 # Messaggio di log per l'anteprima del GeoDataFrame
+    logging.info(f"Clean Data: Shape del Geodataframe: {gdf.shape}")                                                                                # Messaggio di log per la forma del GeoDataFrame
+    logging.info(f"Clean Data: Conteggio per tipologia record: \n\n{gdf['dtipologia2'].value_counts()}\n\n\n")                                      # Messaggio di log per il conteggio per tipologia record
+
+
+
+    '''3. FASE DI PULIZIA DEL FILE PARQUET-------------------------------------------------------------------------------------------------------'''
+
+    logging.info("########Clean Data: Inizio della fase di pulizia del file parquet################################################################")
+    logging.info("Eliminazione delle colonne 'geo_point_2d','duso','length' in quanto ridondanti.")
+    columns_to_drop = ['geo_point_2d','duso','length']
+    gdf = gdf.drop(columns=columns_to_drop)                                                                                                         # Elimino le colonne definite in lista
+
+    logging.info("Concatenazione di un campo unico della tipologia ed eliminazione delle colonne utilizzate.")
+    gdf['tipologia'] = gdf['dtipologia2'] + ' - ' + gdf['tipologia2']                                                                               # Concateno i due campi in un unico
+    gdf = gdf.drop(columns=['dtipologia2','tipologia2'])                                                                                            # Elimino i campi non piu utili utilizzati prima
+    logging.info(f"DataFrame risultante: \n {tabulate(gdf.head(), headers='keys', tablefmt='psql')}\n\n\n")
+
+
+    '''4. RIDENOMINAZIONE COLONNE----------------------------------------------------------------------------------------------------------------'''
+
+    logging.info("########Clean Data: Ridenominazione delle colonne ##############################################################################")
+    logging.info("Rinomino le colonne.")
+    gdf = gdf.rename(columns={'codice':'code',                                                                                                      # Definisco un dizionario che rinomina le colonne
+                              'anno':'year_of_data',
+                              'lunghezza':'length_meters',
+                              'tipologia':'type',
+                              'nomequart':'neighborhood_name',
+                              'zona_fiu':'zone_name'})
+
+    logging.info(f"DataFrame post ridenominazione:\n{tabulate(gdf.head(), headers='keys',tablefmt='psql')}\n\n\n")
+
+
+    '''5. GESTIONE VALORI MANCANTI---------------------------------------------------------------------------------------------------------------'''
+
+    logging.info("########Clean Data: Gestione Valori Mancanti ###################################################################################")
+    logging.info("Elaboriamo per le colonne 'year_of_data','type' l'eliminazione delle righe qualora troviamo valori null")
+    logging.info(f"Numero record pre-eliminazione: {gdf.shape[0]}")                                                                                 # Estrazione dei record pre eliminazione
+    gdf_post = gdf.dropna(subset=['type','year_of_data'])                                                                                           # Definizione DataFrame con eliminazione
+    logging.info(f"Numero record post-eliminazione : {gdf_post.shape[0]}")
+    concat = pd.concat([gdf, gdf_post])                                                                                                             # Concateniamo i record dei 2 Dataframe (pre e post)
+    delete_record = concat.drop_duplicates(keep=False)                                                                                              # Eliminiamo i duplicati (cio' che resta sono gli eliminati)
+    logging.info(f"Record eliminati: \n{tabulate(delete_record, headers='keys', tablefmt='psql')}")
+
--- src/ingest_data.py
+++ src/ingest_data.py
@@ -6,7 +6,7 @@ import pandas as pd
 import logging
 import sys
 
-def ingest_geojson(path_file: str) -> geopandas.GeoDataFrame:                                                           # Funzione per caricare un file geojson in un GeoDataFrame       
+def ingest_geojson(path_file: str) -> geopandas.GeoDataFrame:                                                                                       # Funzione per caricare un file geojson in un GeoDataFrame       
     """
     Funzione per caricare un file geojson in un GeoDataFrame
     :param path_file: percorso del file geojson
@@ -14,15 +14,15 @@ def ingest_geojson(path_file: str) -> geopandas.GeoDataFrame:                   
     """
     
     if not os.path.exists(path_file):
-        logging.error(f"Ingest_data: File {path_file} non trovato.")                                                    # Controlla se il file esiste
+        logging.error(f"Ingest_data: File {path_file} non trovato.")                                                                                # Controlla se il file esiste
         raise FileNotFoundError(f"Ingest_data: File {path_file} non trovato.")
     
     logging.info(f"Ingest_data: Caricamento del file {path_file}...")
     
     try:
-        gdf = geopandas.read_file(path_file)                                                                            # Carica il file geojson in un GeoDataFrame
+        gdf = geopandas.read_file(path_file)                                                                                                        # Carica il file geojson in un GeoDataFrame
     except Exception as e:
-        logging.exception(f"Ingest_data: Errore durante il caricamento del file {path_file}")                           # Gestione degli errori di lettura del file
+        logging.exception(f"Ingest_data: Errore durante il caricamento del file {path_file}")                                                       # Gestione degli errori di lettura del file
         raise e
     logging.info(f"Ingest_data: File {path_file} caricato con successo.")
     
@@ -34,12 +34,12 @@ def ingest_geojson(path_file: str) -> geopandas.GeoDataFrame:                   
 if __name__ == "__main__":
     
     FILE_NAME = 'piste-ciclopedonali.geojson'
-    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))                                                             # Ottieni la directory dello script corrente
-    PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)                                                                          # Ottieni la directory principale del progetto (directory padre dello script)
-    PATH = os.path.join(PROJECT_ROOT,'data','raw',FILE_NAME)                                                            # Costruisci il percorso completo del file 
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))                                                                                         # Ottieni la directory dello script corrente
+    PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)                                                                                                      # Ottieni la directory principale del progetto (directory padre dello script)
+    PATH = os.path.join(PROJECT_ROOT,'data','raw',FILE_NAME)                                                                                        # Costruisci il percorso completo del file 
 
     LOG_FILE = 'pipeline.log'
-    LOG_PATH = os.path.join(PROJECT_ROOT, 'logs', LOG_FILE)                                                             # Costruisci il percorso completo del file di log
+    LOG_PATH = os.path.join(PROJECT_ROOT, 'logs', LOG_FILE)                                                                                         # Costruisci il percorso completo del file di log
 
     
     logging.basicConfig(
@@ -47,21 +47,21 @@ if __name__ == "__main__":
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
-        filemode='w'                                                                                                    # Modalità di apertura del file di log (scrittura)
+        filemode='w'                                                                                                                                # Modalità di apertura del file di log (scrittura)
     )
     
     
-    logging.info("Ingest_data: Inizio dell'esecuzione dello script.")                                                   # Messaggio di inizio dello script  
+    logging.info("Ingest_data: Inizio dell'esecuzione dello script.")                                                                               # Messaggio di inizio dello script  
     
     '''1. FASE DI CARICAMENTO DEL FILE GEOJSON'''
     
     try:
-        gdf = ingest_geojson(PATH)                                                                                      # Carica il file geojson in un GeoDataFrame
+        gdf = ingest_geojson(PATH)                                                                                                                  # Carica il file geojson in un GeoDataFrame
     except FileNotFoundError as e:
-        logging.exception(f"Ingest_data: File non trovato: {e}")                                                        # Gestione dell'eccezione se il file non viene trovato
+        logging.exception(f"Ingest_data: File non trovato: {e}")                                                                                    # Gestione dell'eccezione se il file non viene trovato
         sys.exit(1)
     except Exception as e:
-        logging.exception(f"Ingest_data: Errore durante l'esecuzione dello script: {e}")                                # Gestione di altre eccezioni
+        logging.exception(f"Ingest_data: Errore durante l'esecuzione dello script: {e}")                                                            # Gestione di altre eccezioni
         sys.exit(1)
     
     
@@ -72,12 +72,12 @@ if __name__ == "__main__":
     logging.info("\n\n\n########Ingest_data: File geojson caricato con successo - VERIFICA FILE########################################")
     
     if  gdf.shape[0] == 0:
-        logging.warning("Ingest_data: Il file geojson è vuoto.")                                                        # Messaggio di errore se il GeoDataFrame è vuoto
+        logging.warning("Ingest_data: Il file geojson è vuoto.")                                                                                    # Messaggio di errore se il GeoDataFrame è vuoto
     if gdf.shape[1] < 2:
-        logging.error("Ingest_data: Il file geojson non contiene colonne sufficienti.")                                 # Messaggio di errore se il GeoDataFrame non ha colonne sufficienti
+        logging.error("Ingest_data: Il file geojson non contiene colonne sufficienti.")                                                             # Messaggio di errore se il GeoDataFrame non ha colonne sufficienti
         sys.exit(1)
     if gdf.isnull().sum().sum() > 0:
-        logging.warning("Ingest_data: Il file geojson contiene valori nulli.")                                          # Messaggio di errore se il GeoDataFrame contiene valori nulli
+        logging.warning("Ingest_data: Il file geojson contiene valori nulli.")                                                                      # Messaggio di errore se il GeoDataFrame contiene valori nulli
     
     logging.info("\n\n\n########Ingest_data: File geojson FINE VERIFICA FILE##########################################################\n\n\n")
         
@@ -86,16 +86,16 @@ if __name__ == "__main__":
     
     '''3. FASE DI SALVATAGGIO DEL FILE GEOJSON IN PARQUET'''    
         
-    OUTPUT_FILENAME_PARQUET = FILE_NAME.replace('.geojson','.parquet')                                                   # Nome del file di output in formato parquet
+    OUTPUT_FILENAME_PARQUET = FILE_NAME.replace('.geojson','.parquet')                                                                              # Nome del file di output in formato parquet
     OUTPUT_PARQUET_PATH = os.path.join(PROJECT_ROOT,'data','raw',OUTPUT_FILENAME_PARQUET)    
     
     try:    
-        gdf.to_parquet(OUTPUT_PARQUET_PATH, index=False)                                                                # Salva il GeoDataFrame in un file parquet
-        logging.info(f"Ingest_data: File parquet salvato con successo in {OUTPUT_PARQUET_PATH}")                        # Messaggio di log per il salvataggio del file parquet
+        gdf.to_parquet(OUTPUT_PARQUET_PATH, index=False)                                                                                            # Salva il GeoDataFrame in un file parquet
+        logging.info(f"Ingest_data: File parquet salvato con successo in {OUTPUT_PARQUET_PATH}")                                                    # Messaggio di log per il salvataggio del file parquet
     
     except Exception as e:
-        logging.exception(f"Ingest_data: Errore durante il salvataggio del file parquet: {e}")                          # Gestione degli errori di salvataggio del file parquet
+        logging.exception(f"Ingest_data: Errore durante il salvataggio del file parquet: {e}")                                                      # Gestione degli errori di salvataggio del file parquet
         sys.exit(1)
         
-    logging.info("Ingest_data: Esecuzione dello script completata.")                                                    # Messaggio di completamento dello script
+    logging.info("Ingest_data: Esecuzione dello script completata.")                                                                                # Messaggio di completamento dello script
    
